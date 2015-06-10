49feec8c09a51a35f78d542931f9d14d63f54b51
ATTY Lionel <lionel.atty@ign.fr>
2015-06-10 11:24:03 +0200

    fix: libQGLViewer du depot git n'etait pas dans le depot

---
+++ qprog/libs/qglviewer/libQGLViewer_from_git/examples/luxo/main.cpp
@@ -0,0 +1,15 @@
+#include "luxo.h"
+#include <qapplication.h>
+
+int main(int argc, char** argv)
+{
+  QApplication application(argc,argv);
+
+  Viewer viewer;
+
+  viewer.setWindowTitle("luxo");
+
+  viewer.show();
+
+  return application.exec();
+}
---
+++ qprog/libs/qglviewer/libQGLViewer_from_git/examples/overpainting/main.cpp
@@ -0,0 +1,13 @@
+#include "viewer.h"
+#include <qapplication.h>
+
+int main(int argc, char** argv)
+{
+  QApplication application(argc,argv);
+  Viewer viewer;
+
+  viewer.setWindowTitle("overpainting");
+
+  viewer.show();
+  return application.exec();
+}
